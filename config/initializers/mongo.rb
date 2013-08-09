fd3c2b5e0db7b3e7f2417804768771a735ac898d
Andreas Sprotte-Hansen <andreassprotte-hansen@andreassprottelocal.local>
2013-08-09 15:59:06 +0200

    distinguis db connection on production and development

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,7 +1,9 @@
-# for local env:
-#MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-# for production env:
-MongoMapper.connection = Mongo::Connection.new('195.231.85.192',27017)
+if Rails.env="production"
+  MongoMapper.connection = Mongo::Connection.new('195.231.85.192',27017) # production env
+else
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017) # local env
+end
+
 MongoMapper.database = "#myapp-#{Rails.env}"
 
 if defined?(PhusionPassenger)
