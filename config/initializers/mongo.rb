afe163ddfc096759885ad959d0d9c7372dc63f93
Andreas Sprotte-Hansen <andreassprotte-hansen@andreassprottelocal.local>
2013-08-09 15:54:24 +0200

    production mode works

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,7 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# for local env:
+#MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# for production env:
+MongoMapper.connection = Mongo::Connection.new('195.231.85.192',27017)
 MongoMapper.database = "#myapp-#{Rails.env}"
 
 if defined?(PhusionPassenger)
