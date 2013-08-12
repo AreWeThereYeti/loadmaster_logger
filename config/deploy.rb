d2226dbcd6fbc950cffdcd0adce93d08fff4ed08
Andreas Sprotte-Hansen <andreassprotte-hansen@andreassprottelocal.local>
2013-08-12 13:41:08 +0200

    passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,3 +21,12 @@ set :use_sudo, false
 load 'deploy/assets'
 
 set :rails_env, "production"
+
+# Passenger
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
