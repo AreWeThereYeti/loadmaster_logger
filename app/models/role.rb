d4a9aa2611ed103637148604eb0ffa636fef310a
Andreas Sprotte-Hansen <andreassprotte-hansen@andreassprottelocal.local>
2013-09-25 17:56:52 +0200

    check if role is nil

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,8 +7,10 @@ class Role
   field :name,              :type => String, :default => ""
   
   def check_if_exists
-    if !Role.where(:name => self[:name]).first.nil?
-      return false
+    if !Role.where(:name => self[:name]).nil? 
+      if !Role.where(:name => self[:name]).first.nil?
+        return false
+      end
     end
   end
   
