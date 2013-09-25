6a470723fe9b0b5b47419be505a1759a39aeb294
Andreas Sprotte-Hansen <andreassprotte-hansen@andreassprottelocal.local>
2013-09-25 17:41:36 +0200

    no conneciton mumbojumbo

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,13 +1,13 @@
-if Rails.env=="production"
-  Mongoid.database = Mongoid::Connection.new('195.231.85.192',27017) # production env
-else
-  Mongoid.database = Mongoid::Connection.new('localhost', 27017) # local env
-end
-
-Mongoid.database = "#myapp-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-  PhusionPassenger.on_event(:starting_worker_process) do |forked|
-    Mongoid.connection.connect if forked
-  end
-end
+# if Rails.env=="production"
+#   Mongoid.database = Mongoid::Connection.new('195.231.85.192',27017) # production env
+# else
+#   Mongoid.database = Mongoid::Connection.new('localhost', 27017) # local env
+# end
+# 
+# Mongoid.database = "#myapp-#{Rails.env}"
+# 
+# if defined?(PhusionPassenger)
+#   PhusionPassenger.on_event(:starting_worker_process) do |forked|
+#     Mongoid.connection.connect if forked
+#   end
+# end
