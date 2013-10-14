f88e5b598ffe6ef4cbf9d9ba4818c1d9fa13dd75
Andreas Sprotte-Hansen <andreassprotte-hansen@andreassprottelocal.local>
2013-10-14 14:01:49 +0200

    no-precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ LoadmasterLogger::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += [ 'pdf.css' ]
+  #config.assets.precompile += [ 'pdf.css' ]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
