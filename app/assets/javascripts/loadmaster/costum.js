d5ca8b254559e094a10466407c8a37f999307522
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Air.local>
2013-10-15 14:19:03 +0200

    Datepicker and styling

--- app/assets/javascripts/loadmaster/costum.js
+++ app/assets/javascripts/loadmaster/costum.js
@@ -1,9 +1,23 @@
-// $(document).ready(function(){
+/*
+$(document).ready(function(){
+	
+	$(function() {
+		$( ".datepicker" ).datepicker();
+	});
+*/ 
+ 
 // 	 fontDetect.onFontLoaded ('MyCoolFont', function(){
 // 			console.log('font loaded	')
 // 		}, function(){
 // 			console.log('font not loaded')
 // 		}, {msTimeout: 3000});
-// })
-// 
-// 
+//})
+
+(function($) {
+    $(document).ready(function(){
+        $(".datepicker").datepicker();
+    });
+})(jQuery);
+ 
+ 
+
