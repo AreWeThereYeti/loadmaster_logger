3afd2a0aef86b57b0547a4f15b547a484ac4d57e
Andreas Sprotte-Hansen <andreas@sprotte.dk>
2015-03-25 22:53:23 +0100

    setting correct capistrano IPs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server "195.231.85.191", :app, :web, :db, :primary => true
+server "83.136.91.57", :app, :web, :db, :primary => true
 set :deploy_to, '/home/sprotte/www/loadmaster_logger'
 set :branch, 'master'
